1c4b973518731bd1a848d616d6b8a98695d60941
Peyton <74024201+veechi@users.noreply.github.com>
2024-03-15 20:33:34 -0400

    Formatting

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-#TODO: DONE! 
 #TODO: Find a way to have a user delete a row from the data.csv file
 #TODO: Find a way to have a user ADD a row to the data.csv file
-#TODO: Implement Kivy GUI
 #TODO: Find out how to format email into html code??
 
 from backend_class import Backend
@@ -11,8 +9,13 @@ import datetime
 today = str(datetime.date.today())
 
 backend = Backend
+
 backend.read_csv(backend)
+
 print(backend.csv_data)
+
 backend.update_data(backend)
+
 backend.write_csv(backend)
+
 backend.send_email(backend)
